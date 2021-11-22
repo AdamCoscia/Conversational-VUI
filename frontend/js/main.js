47426662bb61887abee8f5d66ea799c525fa78b0
Adam Coscia <47363758+AdamCoscia@users.noreply.github.com>
2021-11-21 20:22:31 -0500

    add events

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,6 +3,9 @@
  * Georgia Tech, Fall 2021
  * Adam Coscia
  *
+ * TODO:
+ *   - Separate alarms/events into AlarmManager.js and EventManager.js
+ *
  * References:
  *   - Web Speech API: https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API
  *   - SpeechSynthesis: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
@@ -60,21 +63,41 @@ var alarmClockAudio = new Audio("./assets/audio/alarm-clock-01.wav");
 
 // initialize data objects
 var alarms = {};
-var calendar = {};
+var events = {};
+var eventID = 1;
 
 // test alarms
+const t1 = new Date(2021, 10, 17, 21, 48, 0);
+const label1 = "my alarm 1";
+setAlarm(t1, label1, true);
+const t2 = new Date(2021, 10, 17, 23, 48, 0);
+const label2 = "";
+setAlarm(t2, label2, true);
+setTimeout(() => editAlarmEnabled(t2, "", false), 5000);
+setTimeout(() => editAlarmLabel(t2, "my alarm 1", "my alarm 2"), 10000);
+const t3 = new Date(2021, 10, 17, 23, 48, 30);
+setTimeout(() => editAlarmTime(t1, "my alarm 2", t3), 15000);
+setTimeout(() => deleteAlarm(null, "my alarm 2"), 20000);
+setTimeout(() => console.log("done!"), 25000);
+
+// test events
 // const t1 = new Date(2021, 10, 17, 21, 48, 0);
-// const label1 = "my alarm 1";
-// setAlarm(t1, label1, true);
+// const label1 = "my event 1";
+// const d1 = 120; // minutes
+// setEvent(t1, label1, d1, true);
 // const t2 = new Date(2021, 10, 17, 23, 48, 0);
 // const label2 = "";
-// setAlarm(t2, label2, true);
-// setTimeout(() => editAlarmEnabled(t2, "", false), 10000);
-// setTimeout(() => editAlarmLabel(t2, "my alarm 1", "my alarm 2"), 15000);
+// const d2 = 240; // minutes
+// setEvent(t2, label2, d2, true);
+// setTimeout(() => editEventLabel(t2, "my event 1", "my event 2"), 5000);
 // const t3 = new Date(2021, 10, 17, 23, 48, 30);
-// setTimeout(() => editAlarmTime(t1, "my alarm 2", t3), 15000);
-// setTimeout(() => deleteAlarm(null, "my alarm 2"), 20000);
+// const d3 = 180; // minutes
+// setTimeout(() => editEventTime(t1, "my event 2", t3), 10000);
+// setTimeout(() => deleteEvent(null, "my event 2"), 15000);
+// setTimeout(() => editEventDuration(null, "my event 1", d3), 20000);
 // setTimeout(() => console.log("done!"), 25000);
+
+/** ========================== INITIALIZATION ============================== */
 
 // attempt to start the system
 if (annyang) {
@@ -235,26 +258,28 @@ function setCallbacks() {
   });
 }
 
+/** ============================== ALARMS ================================== */
+
 /**
  * Set an alarm for the time given with the given label and with the enabled
  * flag set to the given value.
  * @param {Date} time Time to set the alarm for.
- * @param {String} label A label for the alarm.
+ * @param {String} label (optional) A label for the alarm.
  * @param {Boolean} enabled A bool for whether alarm should be enabled.
  */
 function setAlarm(time, label, enabled) {
-  if (time && !Object.hasOwn(alarms, time)) {
-    const ts = Math.floor(time.getTime() / 1000); // get seconds
+  if (time && !Object.hasOwn(alarms, time.getTime())) {
+    const ts = Math.floor(time.getTime() / 1000); // seconds
     function testAlarm() {
       const d = new Date();
-      const ds = Math.floor(d.getTime() / 1000); // get seconds
+      const ds = Math.floor(d.getTime() / 1000); // seconds
       if (ts == ds) {
-        if (alarms[time].enabled) alarmClockAudio.play(); // play the alarm!
-        window.clearInterval(alarms[time].id); // stop testing this alarm
+        if (alarms[time.getTime()].enabled) alarmClockAudio.play(); // play the alarm!
+        window.clearInterval(alarms[time.getTime()].id); // stop testing this alarm
       }
     }
     var testAlarmIntervalID = window.setInterval(testAlarm, 1000); // test alarm every second
-    alarms[time] = { id: testAlarmIntervalID, time: time, label: label, enabled: enabled }; // save alarm by time
+    alarms[time.getTime()] = { id: testAlarmIntervalID, time: time, label: label, enabled: enabled }; // save alarm by time
     if (label && !Object.hasOwn(alarms, label)) {
       alarms[label] = { id: testAlarmIntervalID, time: time, label: label, enabled: enabled }; // save alarm by label
     }
@@ -268,16 +293,18 @@ function setAlarm(time, label, enabled) {
  * @param {String} label Label of the alarm to delete.
  */
 function deleteAlarm(time, label) {
-  if (time && Object.hasOwn(alarms, time)) {
-    window.clearInterval(alarms[time].id); // stop testing this alarm
-    if (alarms[time].label && Object.hasOwn(alarms, alarms[time].label)) {
-      delete alarms[alarms[time].label]; // remove alarm by label
+  if (time && Object.hasOwn(alarms, time.getTime())) {
+    window.clearInterval(alarms[time.getTime()].id); // stop testing this alarm
+    const oldLabel = alarms[time.getTime()].label;
+    if (oldLabel && Object.hasOwn(alarms, oldLabel)) {
+      delete alarms[oldLabel]; // remove alarm by label
     }
-    delete alarms[time]; // remove alarm by time
+    delete alarms[time.getTime()]; // remove alarm by time
   } else if (label && Object.hasOwn(alarms, label)) {
     window.clearInterval(alarms[label].id); // stop testing this alarm
-    if (alarms[label].time && Object.hasOwn(alarms, alarms[label].time)) {
-      delete alarms[alarms[label].time]; // remove alarm by time
+    const oldTime = alarms[label].time;
+    if (oldTime && Object.hasOwn(alarms, oldTime.getTime())) {
+      delete alarms[oldTime.getTime()]; // remove alarm by time
     }
     delete alarms[label]; // remove alarm by label
   }
@@ -292,16 +319,16 @@ function deleteAlarm(time, label) {
  * @param {Date} newTime New time to give to the alarm.
  */
 function editAlarmTime(time, label, newTime) {
-  if (time && Object.hasOwn(alarms, time)) {
-    const oldLabel = alarms[time].label;
-    const enabled = alarms[time].enabled;
-    deleteAlarm(time, label);
-    setAlarm(newTime, oldLabel, enabled);
+  if (time && Object.hasOwn(alarms, time.getTime())) {
+    const oldLabel = alarms[time.getTime()].label;
+    const oldEnabled = alarms[time.getTime()].enabled;
+    deleteAlarm(time, oldLabel);
+    setAlarm(newTime, oldLabel, oldEnabled);
   } else if (label && Object.hasOwn(alarms, label)) {
-    const oldLabel = alarms[label].label;
-    const enabled = alarms[label].enabled;
-    deleteAlarm(time, label);
-    setAlarm(newTime, oldLabel, enabled);
+    const oldTime = alarms[label].time;
+    const oldEnabled = alarms[label].enabled;
+    deleteAlarm(oldTime, label);
+    setAlarm(newTime, label, oldEnabled);
   }
   console.log(alarms);
 }
@@ -314,41 +341,147 @@ function editAlarmTime(time, label, newTime) {
  * @param {String} newLabel New label to give to the alarm.
  */
 function editAlarmLabel(time, label, newLabel) {
-  if (time && Object.hasOwn(alarms, time)) {
-    const enabled = alarms[time].enabled;
-    deleteAlarm(time, label);
-    setAlarm(time, newLabel, enabled);
+  if (time && Object.hasOwn(alarms, time.getTime())) {
+    const oldLabel = alarms[time.getTime()].label;
+    const oldEnabled = alarms[time.getTime()].enabled;
+    deleteAlarm(time, oldLabel);
+    setAlarm(time, newLabel, oldEnabled);
   } else if (label && Object.hasOwn(alarms, label)) {
-    const enabled = alarms[label].enabled;
-    deleteAlarm(time, label);
-    setAlarm(time, newLabel, enabled);
+    const oldTime = alarms[label].time;
+    const oldEnabled = alarms[label].enabled;
+    deleteAlarm(oldTime, label);
+    setAlarm(oldTime, newLabel, oldEnabled);
   }
   console.log(alarms);
 }
 
 /**
- * Sets the `enabled` prop of an alarm with the given time, or if the time is not
+ * Sets the `label` prop of an alarm with the given time, or if the time is not
  * give, label, to the given value.
  * @param {Date} time Time of the alarm to edit.
  * @param {String} label Label of the alarm to edit.
- * @param {Boolean} enabled A bool for whether alarm should be enabled.
+ * @param {String} newEnabled Whether to enable alarm or not.
  */
-function editAlarmEnabled(time, label, enabled) {
-  if (time && Object.hasOwn(alarms, time)) {
-    const alarm = alarms[time];
-    alarm.enabled = enabled;
-    if (alarm.label && Object.hasOwn(alarms, alarm.label)) {
-      alarms[alarm.label].enabled = enabled;
-    }
+function editAlarmEnabled(time, label, newEnabled) {
+  if (time && Object.hasOwn(alarms, time.getTime())) {
+    const oldLabel = alarms[time.getTime()].label;
+    deleteAlarm(time, oldLabel);
+    setAlarm(time, oldLabel, newEnabled);
   } else if (label && Object.hasOwn(alarms, label)) {
-    const alarm = alarms[label];
-    alarm.enabled = enabled;
-    if (alarm.time && Object.hasOwn(alarms, alarm.time)) {
-      alarms[alarm.time].enabled = enabled;
-    }
+    const oldTime = alarms[label].time;
+    deleteAlarm(oldTime, label);
+    setAlarm(oldTime, label, newEnabled);
   }
   console.log(alarms);
 }
+
+/** ============================== EVENTS ================================== */
+
+/**
+ * Set an event for the time given with the given label.
+ * @param {Date} time Time to set the event for.
+ * @param {String} label (optional) A label for the event.
+ * @param {*} duration How long the event runs, in minutes.
+ */
+function setEvent(time, label, duration) {
+  if (time && !Object.hasOwn(events, time.getTime())) {
+    eventID++; // get new event ID
+    events[time.getTime()] = { id: eventID, time: time, label: label, duration: duration }; // save event by time
+    if (label && !Object.hasOwn(events, label)) {
+      events[label] = { id: eventID, time: time, label: label, duration: duration }; // save event by label
+    }
+  }
+  console.log(events);
+}
+
+/**
+ * Deletes the event with the given time, or if time is not given, label.
+ * @param {Date} time Time of the event to delete.
+ * @param {String} label Label of the event to delete.
+ */
+function deleteEvent(time, label) {
+  if (time && Object.hasOwn(events, time.getTime())) {
+    const oldLabel = events[time.getTime()].label;
+    if (oldLabel && Object.hasOwn(events, oldLabel)) {
+      delete events[oldLabel]; // remove event by label
+    }
+    delete events[time.getTime()]; // remove event by time
+  } else if (label && Object.hasOwn(events, label)) {
+    const oldTime = events[label].time;
+    if (oldTime && Object.hasOwn(events, oldTime.getTime())) {
+      delete events[oldTime.getTime()]; // remove event by time
+    }
+    delete events[label]; // remove event by label
+  }
+  console.log(events);
+}
+
+/**
+ * Sets the `time` prop of an event with the given time, or if the time is not
+ * give, label, to the given value.
+ * @param {Date} time Time of the event to edit.
+ * @param {String} label Label of the event to edit.
+ * @param {Date} newTime New time to give to the event.
+ */
+function editEventTime(time, label, newTime) {
+  if (time && Object.hasOwn(events, time.getTime())) {
+    const oldLabel = events[time.getTime()].label;
+    const oldDuration = events[time.getTime()].duration;
+    deleteEvent(time, oldLabel);
+    setEvent(newTime, oldLabel, oldDuration);
+  } else if (label && Object.hasOwn(events, label)) {
+    const oldTime = events[label].time;
+    const oldDuration = events[label].duration;
+    deleteEvent(oldTime, label);
+    setEvent(newTime, label, oldDuration);
+  }
+  console.log(events);
+}
+
+/**
+ * Sets the `label` prop of an event with the given time, or if the time is not
+ * give, label, to the given value.
+ * @param {Date} time Time of the event to edit.
+ * @param {String} label Label of the event to edit.
+ * @param {String} newLabel New label to give to the event.
+ */
+function editEventLabel(time, label, newLabel) {
+  if (time && Object.hasOwn(events, time.getTime())) {
+    const oldLabel = events[time.getTime()].label;
+    const oldDuration = events[time.getTime()].duration;
+    deleteEvent(time, oldLabel);
+    setEvent(time, newLabel, oldDuration);
+  } else if (label && Object.hasOwn(events, label)) {
+    const oldTime = events[label].time;
+    const oldDuration = events[label].duration;
+    deleteEvent(oldTime, label);
+    setEvent(oldTime, newLabel, oldDuration);
+  }
+  console.log(events);
+}
+
+/**
+ * Sets the `time` prop of an event with the given time, or if the time is not
+ * give, label, to the given value.
+ * @param {Date} time Time of the event to edit.
+ * @param {String} label Label of the event to edit.
+ * @param {Date} newDuration New duration to give the event.
+ */
+function editEventDuration(time, label, newDuration) {
+  if (time && Object.hasOwn(events, time.getTime())) {
+    const oldLabel = events[time.getTime()].label;
+    deleteEvent(time, oldLabel);
+    setEvent(time, oldLabel, newDuration);
+  } else if (label && Object.hasOwn(events, label)) {
+    const oldTime = events[label].time;
+    const oldLabel = events[label].label;
+    deleteEvent(oldTime, label);
+    setEvent(oldTime, oldLabel, newDuration);
+  }
+  console.log(events);
+}
+
+/** ================================ I/O =================================== */
 
 /**
  * Listen to the user's voice using the speech recognition API.
